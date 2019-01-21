7a44dbb789014db2fa16686efd5e6c75fc4a6c66
Jonathan Lima <jlimahaverford@gmail.com>
2019-01-20 23:56:53 -0500

    Added login, registration, and profile pages.

--- amp.py
+++ amp.py
@@ -1,2 +1,12 @@
-from app import app
+from app import app, db
+from app.models import User, TwitterUser, Tweet, Amp
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db, 
+        'User': User, 
+        'TwitterUser': TwitterUser, 
+        'Tweet': Tweet,
+        'Amp': Amp}
 
