5310b8a6f30db2d06b80bb21d7597743f0a85bea
Jonathan Lima <jlimahaverford@gmail.com>
2019-01-20 19:37:46 -0500

    added TwitterUser/Tweet/Amp objects and tables.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,7 +6,36 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    amps = db.relationship('Amp', backref='amper', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
+class TwitterUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    twitter_username = db.Column(db.String(15), index=True, unique=True)
+    tweets = db.relationship('Tweet', backref='tweeter', lazy='dynamic')
+
+    def __repr__(self):
+        return '<TwitterUser {}>'.format(self.twitter_username)
+
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    twitter_user_id = db.Column(db.Integer, db.ForeignKey('twitter_user.id'))
+    body = db.Column(db.String(280))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.body)
+
+
+class Amp(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweet.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Amp {}, {}>'.format(self.user_id, self.tweet_id)
